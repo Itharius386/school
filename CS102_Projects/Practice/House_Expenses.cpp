f7ec9a4b90a9cc0053c27f0a7433413a35615a69
James <jemarcum386@gmail.com>
2018-01-23 14:48:52 -0500

    Header and expense
    starting working on header and house expense problem from book

---
+++ CS102_Projects/Practice/House_Expenses.cpp
@@ -0,0 +1,31 @@
+//shows monthly and annual costs for given catagories of expenses
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+
+//expense class - takes in type and amount/month
+class Expense{
+  private:
+//total for month
+    double total_mnth;
+//name of expense
+    string e_name;
+  public:
+      Expense(double amount, string name){total_mnth = amount; e_name = name;}
+      ~Expense()
+
+      double getMonth(){return total_mnth;}
+      double getYear(){return total_mnth*12;}
+      double getDay(){return total_mnth*12/365;}
+      string getName(){return e_name;}
+}
+
+int main(){
+
+
+
+
+  return 0;
+}
