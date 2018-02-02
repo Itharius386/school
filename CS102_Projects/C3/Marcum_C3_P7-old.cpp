6f9a39aae63747cf3696b14f10263ac3bcb72241
James <jemarcum386@gmail.com>
2018-02-01 22:41:51 -0500

    Updating P7
    P7 mostly finished. Finishing up on the output table. -old is the form prior to class addition

---
+++ CS102_Projects/C3/Marcum_C3_P7-old.cpp
@@ -0,0 +1,118 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                         *
+ *    Author: James Marcum                                                                 *
+ *    Class:  CS102                                                                        *
+ *                                                                                         *
+ *    Function:   Box office calculator                                                    *
+ *                Gives box office profit from tickets sold                                *
+ *                and how much is paid back to the distributor                             *
+ *                                                                                         *
+ *    Default:    Adult: $10 | Child: $6 | Retained: 20%                                   *
+ *                                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+//Headers
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include "marcum_header.h"
+#include "marcum_header.cpp"
+//Namespace
+using namespace std;
+
+//Prototypes
+
+//class
+//a movie has a title, and can reference the number of Tickets sold
+//Also can be 3D for a theatre surcharge, and has a rating for analytic purpose
+//it won't have an individual price/ticket, as that is set by the theatre/distributor
+class Movie{
+  private:
+    string title;
+    int a_ticket_sold, c_ticket_sold, is3D, rating;
+  public:
+    //constructor
+    Movie(string name,int a_tic=0,int c_tic=0,int _3d=0,int rated=3)
+      {title = name; a_ticket_sold = a_tic; c_ticket_sold = c_tic; is3D = _3d; rated = rating;}
+    //gets
+    string getTitle(){return title;}
+    int getATicket(){return a_ticket_sold;}
+    int getCTicket(){return c_ticket_sold;}
+    int get3D(){return is3D;}
+    int getRating(){return rating;}
+    //what is better: having them take the input as an arguement or have them with 0 arguements and call for their own input with input checking built in to prevent user error?
+    void setTitle(string name){title = name;}
+    void setATicket(int tic){a_ticket_sold = tic;}
+    void setCTicket(int tic){c_ticket_sold = tic;}
+    void set3D(int set){is3D = set;}
+    void setRating(int set){rating = set;}
+};
+
+//BEGIN MAIN
+int main(){
+//variable inits with default values
+  double a_tic_cost = 10, c_tic_cost = 6, retained_percent = .2, \
+         gross_profit = 0, retained_earn = 0;
+  int a_tickets = 0, c_tickets = 0;
+//loop checks
+  char repeat = 'y', re_check[] = {'y', 'n'};
+  string movie_name;
+//main loop
+while (repeat == 'y'){
+  //display
+  display("Welcome to The Movie Money Generator!","Get all the money you ever dreamed of!!");
+  //Get their Theatre's name
+  //getline(cin >>)
+  //prompt for movie name or settings option
+  cout << "Enter the name of a movie or enter 's' to change the settings: ";
+  //cin >> ws fixes my cin to getline issue of \n being left in the stream if you go to settings, and gets rid of leading whitespace :D
+  getline(cin >> ws, movie_name);
+  //if settings, change settngs (Limits options - Can't name a movie 's')
+  if (movie_name == "s") {
+    cout << "#~~~~~~~~~~#~~~~~~~~~~*Settings*~~~~~~~~~~#~~~~~~~~~~#" << endl;
+    cout << "Price of Adult Tickets (Default $10):     $";
+    d_in(&a_tic_cost,1);
+    //positive check
+    cout << "Price of Child Tickets (Default $6):      $";
+    d_in(&c_tic_cost,1);
+    cout << "Percent of earnings retained (Default 20): ";
+    d_in(&retained_percent,1);
+    //0-100 check
+    while (retained_percent < 0 || retained_percent > 100){
+      cout << "Must be between 0-100: ";
+      d_in(&retained_percent);}
+    //divide entered number by 100 to get it as a percentage
+    retained_percent /= 100;}
+  //else it's the movie title
+  else{
+    //get tickets sold
+    cout << "Enter number of Adult Tickets sold: ";
+    i_in(&a_tickets,1);
+    cout << "Enter number of Child Tickets sold: ";
+    i_in(&c_tickets,1);
+
+    //calculate gross profit and retained earnings (The part you keep)
+    gross_profit = (a_tickets * a_tic_cost + c_tickets * c_tic_cost);
+    retained_earn = gross_profit * retained_percent;
+
+    //show
+    cout << endl << fixed  << setprecision(2) << setw(25) << left << "Movie Title:" << movie_name << endl \
+         << setw(25) << "Adult Tickets Sold:" << a_tickets << endl \
+         << setw(25) << "Child Tickets Sold:" << c_tickets << endl \
+         << setw(25) << "Gross Box Office profit:" << "$" << gross_profit << endl \
+         << setw(25) << "Net Retained Earnings:" << "$" << retained_earn << endl \
+         << setw(25) << "Paid to Distributor:" << "$" << (gross_profit - retained_earn) << endl << endl;}
+  //if they chose settings auto-loop to get movie title otherwise ask if they want to enter another
+  if (movie_name != "s"){
+    cout << "Would you like to enter another movie? (y/n): ";
+    c_in(&repeat,re_check,2);}
+    cin.clear();
+  //clear the screen if they picked yes
+  if (repeat == 'y')
+    system("CLS");
+  }
+  return 0;
+}
+//END MAIN
--- CS102_Projects/C3/Marcum_C3_P7.cpp
+++ CS102_Projects/C3/Marcum_C3_P7.cpp
@@ -17,6 +17,7 @@
 #include <string>
 #include <vector>
 #include <windows.h>
+#include <sstream>
 #include "marcum_header.h"
 #include "marcum_header.cpp"
 //Namespace
@@ -28,104 +29,126 @@ using namespace std;
 //a movie has a title, and can reference the number of Tickets sold
 //Also can be 3D for a theatre surcharge, and has a rating for analytic purpose
 //it won't have an individual price/ticket, as that is set by the theatre/distributor
+//but I let it calculate the revenue from price inputs
 class Movie{
   private:
     string title;
     int a_ticket_sold, c_ticket_sold, is3D, rating;
   public:
-    //constructor
-    Movie(string name,int a_tic=0, c_tic=0, _3d=0, rated=3)
-      {title = name; a_ticket_sold = a_tic; c_ticket_sold = c_tic; is3D = _3d, rated = rating}
+    //constructor - with no arguements acts as a 'wizard' and walksthrough building the showing
+    Movie(){
+        int r_check[]={1,2,3,4,5}, d_check[] = {0,1};
+        cout << "Enter the title: ";
+        getline(cin >> ws,title);
+        cout << "Enter # of Adult Tickets sold: ";
+        i_in(&a_ticket_sold,1);
+        cout << "Enter # of Child Tickets sold: ";
+        i_in(&c_ticket_sold,1);
+        cout << "Is it in 3D? (0 = No, 1 = Yes): ";
+        s_i_in(&is3D,d_check,2);
+        cout << "What is the the movie's rating? (1-5): ";
+        s_i_in(&rating,r_check,5);}
+    Movie(string name,int a_tic=0,int c_tic=0,int _3d=0,int rated=3)
+      {title = name; a_ticket_sold = a_tic; c_ticket_sold = c_tic; is3D = _3d; rated = rating;}
+    //what is better: having them take the input as an arguement or have them with 0 arguements and call for their own input with input checking built in to prevent user error?
+    void setTitle(){
+        cout << "Enter the title: ";
+        getline(cin >> ws,title);}
+    void setATicket(){
+        cout << "Enter # of Adult Tickets sold: ";
+        i_in(&a_ticket_sold,1);}
+    void setCTicket(){
+        cout << "Enter # of Child Tickets sold: ";
+        i_in(&c_ticket_sold,1);}
+    void set3D(){
+        int check[] = {1,2};
+        cout << "Is it in 3D? (0 = No, 1 = Yes): ";
+        s_i_in(&is3D,check,2);}
+    void setRating(){
+        int check[]={1,2,3,4,5};
+        cout << "What is the the movie's rating? (1-5): ";
+        s_i_in(&rating,check,5);}
     //gets
     string getTitle(){return title;}
     int getATicket(){return a_ticket_sold;}
     int getCTicket(){return c_ticket_sold;}
     int get3D(){return is3D;}
+    char show3D(){
+        if (is3D == 1)
+            return 'Y';
+        return 'N';}
     int getRating(){return rating;}
-    //what is better: having them take the input as an arguement or have them with 0 arguements and call for their own input with input checking built in to prevent user error?
-    void setTitle(string name){title = name;}
-    void setATicket(int tic){a_ticket_sold = tic;}
-    void setCTicket(int tic){c_ticket_sold = tic;}
-    void set3D(int set){is3D = set;}
-    void setRating(int set){rating = set;}
+    string showRating(){
+        stringstream star;
+        for (int i = 0; i < rating; i++)
+            star << '*';
+        return star.str();}
+    double calcGross(double priceAdult, double priceChild, double surcharge3D){
+        double total = 0;
+        if (is3D)
+            total += (a_ticket_sold + c_ticket_sold) * surcharge3D;
+        total += a_ticket_sold * priceAdult + c_ticket_sold * priceChild;
+        return total;
+    }
 };
-
-
-class Theatre{
-  private:
-    string theatre;
-    vector<Movie> showings;
-    double adult_tix_price, child_tix_price, charge_3D \
-          retained_percent;
-  public:
-    Theatre(string name = "The Best Theatre You Never Saw", double ap = 10, double cp = 6, double D3 = 3){theatre = name; adult_tix_price = ap; child_tix_price = cp; charge_3D = D3;}
-    void addShowing(string title){showings.push_back(Movie(title));}
-
-}
 
 //BEGIN MAIN
 int main(){
 //variable inits with default values
-  double a_tic_cost = 10, c_tic_cost = 6, retained_percent = .2, \
-         gross_profit = 0, retained_earn = 0;
-  int a_tickets = 0, c_tickets = 0;
+    double a_tic_cost = 10, c_tic_cost = 6, retained_percent = .2, surcharge3D = 3, \
+        profit = 0, t_gross_profit = 0, retained_earn = 0, t_retained = 0;
+    int num_rep,i;
+    string title;
+    vector<Movie> showing;
 //loop checks
-  char repeat = 'y', re_check[] = {'y', 'n'};
-  string movie_name;
-//main loop
-while (repeat == 'y'){
-  //display
-  display("Welcome to The Movie Money Generator!","Get all the money you ever dreamed of!!");
-  //Get their Theatre's name
-  //getline(cin >>)
-  //prompt for movie name or settings option
-  cout << "Enter the name of a movie or enter 's' to change the settings: ";
-  //cin >> ws fixes my cin to getline issue of \n being left in the stream if you go to settings, and gets rid of leading whitespace :D
-  getline(cin >> ws, movie_name);
-  //if settings, change settngs (Limits options - Can't name a movie 's')
-  if (movie_name == "s") {
-    cout << "#~~~~~~~~~~#~~~~~~~~~~*Settings*~~~~~~~~~~#~~~~~~~~~~#" << endl;
-    cout << "Price of Adult Tickets (Default $10):     $";
-    d_in(&a_tic_cost,1);
-    //positive check
-    cout << "Price of Child Tickets (Default $6):      $";
-    d_in(&c_tic_cost,1);
-    cout << "Percent of earnings retained (Default 20): ";
-    d_in(&retained_percent,1);
-    //0-100 check
-    while (retained_percent < 0 || retained_percent > 100){
-      cout << "Must be between 0-100: ";
-      d_in(&retained_percent);}
-    //divide entered number by 100 to get it as a percentage
-    retained_percent /= 100;}
-  //else it's the movie title
-  else{
-    //get tickets sold
-    cout << "Enter number of Adult Tickets sold: ";
-    i_in(&a_tickets,1);
-    cout << "Enter number of Child Tickets sold: ";
-    i_in(&c_tickets,1);
+    char settings = 'a', yn_check[] = {'y', 'n'};
 
-    //calculate gross profit and retained earnings (The part you keep)
-    gross_profit = (a_tickets * a_tic_cost + c_tickets * c_tic_cost);
-    retained_earn = gross_profit * retained_percent;
-
-    //show
-    cout << endl << fixed  << setprecision(2) << setw(25) << left << "Movie Title:" << movie_name << endl \
-         << setw(25) << "Adult Tickets Sold:" << a_tickets << endl \
-         << setw(25) << "Child Tickets Sold:" << c_tickets << endl \
-         << setw(25) << "Gross Box Office profit:" << "$" << gross_profit << endl \
-         << setw(25) << "Net Retained Earnings:" << "$" << retained_earn << endl \
-         << setw(25) << "Paid to Distributor:" << "$" << (gross_profit - retained_earn) << endl << endl;}
-  //if they chose settings auto-loop to get movie title otherwise ask if they want to enter another
-  if (movie_name != "s"){
-    cout << "Would you like to enter another movie? (y/n): ";
-    c_in(&repeat,re_check,2);}
-    cin.clear();
-  //clear the screen if they picked yes
-  if (repeat == 'y')
+    display("Welcome to The Movie Money Generator","Get all the money you ever dreamed of!");
+    cout << "Would you like to change the settings? (y/n): ";
+    c_in(&settings,yn_check,2);
+    if (settings == 'y') {
+        cout << "#~~~~~~~~~~#~~~~~~~~~~*Settings*~~~~~~~~~~#~~~~~~~~~~#" << endl;
+        cout << "Price of Adult Tickets (Default $10):     $";
+        d_in(&a_tic_cost,1);
+        cout << "Price of Child Tickets (Default $6):      $";
+        d_in(&c_tic_cost,1);
+        cout << "Surcharge for 3D (Default $3):            $";
+        d_in(&surcharge3D,1);
+        cout << "Percent of earnings retained (Default 20): ";
+        d_in(&retained_percent,1);
+        //0-100 check
+        while (retained_percent < 0 || retained_percent > 100){
+        cout << "Must be between 0-100: ";
+        d_in(&retained_percent);}
+        //divide entered number by 100 to get it as a percentage
+        retained_percent /= 100;
+        //again only a single chance to change settings per run of the programs
+        system("CLS");
+        display("Welcome to The Movie Money Generator","Get all the money you ever dreamed of!");}
+    //number of movies to enter
+    cout << "How many movies do you want to enter?: ";
+    i_in(&num_rep,1);
+    //main loop
+    for (i = 0; i < num_rep; i++){
+        //make that many movies
+        showing.push_back(Movie());
+        cout << endl;}
     system("CLS");
-  }
+    display("Welcome to The Movie Money Generator","Get all the money you ever dreamed of!");
+    //clear the screen, add the display, then display all the results
+    cout << fixed  << setprecision(2);
+    cout << setw(30) << " " << left << setw(8) << " Adult" << setw(8) << " Child" << setw(10) << "Gross" << setw(9) << "Retained" << setw(12) << " Paid to" << endl;
+    cout << setw(21) << "Title" << setw(6) << "Rated" << setw(3) << "3D" << setw(8) << "Tickets" << setw(8) << "Tickets" << setw(10) << "Profit" << setw(10) << "Earning" << setw(12) << "Distributor" << endl;
+    for (int j = 0; j < 80; j++)
+        cout << "_";
+    cout << endl;
+    for (i = 0; i < num_rep; i++){
+        profit = showing[i].calcGross(a_tic_cost,c_tic_cost,surcharge3D);
+        retained_earn = profit * retained_percent;
+        t_gross_profit += profit;
+        t_retained += retained_earn;
+        cout << setw(21) << showing[i].getTitle() << setw(6) << showing[i].showRating() << setw(3) << showing[i].show3D() << setw(8) << showing[i].getATicket() << setw(8) << showing[i].getCTicket() << "$" << setw(9) << profit << "$" << setw(9) << retained_earn << "$" << setw(11) << profit - retained_earn << endl;
+    }
   return 0;
 }
 //END MAIN
