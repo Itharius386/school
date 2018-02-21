7e07ae7a26308f821e5e7159adc405f014c893e1
James <jemarcum386@gmail.com>
2018-02-20 23:40:56 -0500

    more effort

--- CS102_Projects/C11/Marcum_C11_4.cpp
+++ CS102_Projects/C11/Marcum_C11_4.cpp
@@ -1,84 +1,137 @@
 /*4. Weather Statistics
-Write a program that uses a structure to store the following weather data for a particular
-month:
-Total Rainfall
-High Temperature
-Low Temperature
-Average Temperature
-The program should have an array of 12 structures to hold weather data for an
-entire year. When the program runs, it should ask the user to enter data for each
-month. (The average temperature should be calculated.) Once the data are entered
-for all the months, the program should calculate and display the average monthly
+Once the data are entered for all the months, the program should calculate and display the average monthly
 rainfall, the total rainfall for the year, the highest and lowest temperatures for the
 year (and the months they occurred in), and the average of all the monthly average
 temperatures.
 Input Validation: Only accept temperatures within the range between –100 and +140
 degrees Fahrenheit.
-5. Weather Statistics Modification
-Modify the program that you wrote for Programming Challenge 4 so it defines an
-enumerated data type with enumerators for the months ( JANUARY , FEBRUARY , etc.).
-The program should use the enumerated type to step through the elements of the
-array.*/
+*/
 
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <map>
+#include <time.h>
 #include <algorithm>
 #include "marcum_header.h"
 #include "marcum_header.cpp"
 //namespace
 using namespace std;
 
-enum MONTHS {JANUARY, FEBRUARY, MARCH, APRIL, JUNE, JULY, AUGUST, SEPTEMBER, \
+//month enum
+enum MONTHS {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, \
              OCTOBER, NOVEMBER, DECEMBER};
+//just used for fun
+
+//==============================================================================
+//weather structure
 struct weatherData{
     double rainfall;
     double h_temp;
     double l_temp;
-    //calcs avg temp on demand
-};
+    double avg_temp;
+};//weatherData
 
-//Class
-class Weather{
-private:
-
+//==============================================================================
+//structure to hold 1 year of weather data
+struct annualWeather{
     int year;
-    int month;
-    weatherData data;
-public:
-    Weather(){}
-    Weather(double rf, double htemp, double ltemp){}
-    void setDate(int yr, int mth){year = yr; month = mth;}
-    void setRainfall(double rf){data.rainfall = rf;}
-    void setHTemp(double htemp){data.h_temp = htemp;}
-    void setLTemp(double ltemp){data.l_temp = ltemp;}
-    int getMonth(){return month;}
-    int getYear(){return year;}
-    double getRainfall(){return data.rainfall;}
-    double getHTemp(){return data.h_temp;}
-    double getLTemp(){return data.l_temp;}
-    double getAvgTemp(){return data.h_temp/data.l_temp;}
-};
+    weatherData monthlyData[12];
+};//annualWeather
+//initially did this project with an array of pointers to pointers of arrays
+//but just making a structure that held the years data was much cleaner
 
+//prototype
+string enumMonth(const int mth); //gets a string of the month
+weatherData getWeatherData(const int mth, const char t_type = 'F'); //'set' would be a better term
+void displayWeatherData(const weatherData output, const int mth, const int year);
+
+//BEGIN MAIN
 int main(){
-int year_num;
-//Weather *weather_report[12] = NULL;
+time_t timer = time(NULL);
+struct tm *year = localtime(&timer); //just used to get the year
+int current_year = year->tm_year+1900, \
+    mnths = 12, year_num, year_total; //year_num is year we are itterating on, year total is max years to itterate to
+double rain, hi_temp, lo_temp;
+char temp_type = 'F';
 
-cout << "Weather Report" << endl;
-
+display("Weather Report");
 cout << "How many years of data would you like to enter: ";
-i_in(&year_num,1);
+i_in(&year_total,1);
+year_num = current_year-year_total;
+//sets year to the lowest (ex. 2018-2 = 2016) we are getting data for
+annualWeather *report = new annualWeather[year_total];
+//array of annualWeather points for the number of years we need
 
-map <int, Weather>  
-
-for (int i=0; i<year_num; i++){
-    for (int j=0; j<DECEMBER;j++){
-        cout << "Enter value: ";
-
+for (int year=0; year<year_num; year++){
+    report[year].year = year_num;
+    cout << "Year " << current_year << endl;
+    for (int month=JANUARY; month<=DECEMBER;month++){
+        report[year].monthlyData[month] = getWeatherData(month, temp_type);
+        displayWeatherData(report[year].monthlyData[month], month, year);
     }
+    year_num++; //will end on the current_year
 }
 
 
 return 0;
-}
+} //END MAIN
+
+//returns the month based on a given value from 0-11
+string enumMonth(const int mth){
+    switch(mth){
+        case(JANUARY):return "January";
+        case(FEBRUARY):return "February";
+        case(MARCH):return "March";
+        case(APRIL):return "April";
+        case(MAY):return "May";
+        case(JUNE):return "June";
+        case(JULY):return "July";
+        case(AUGUST):return "August";
+        case(SEPTEMBER):return "September";
+        case(OCTOBER):return "October";
+        case(NOVEMBER):return "November";
+        case(DECEMBER):return "December";
+    }
+}//enumMonth
+
+//gets a weatherData structure
+weatherData getWeatherData(const int mth, const char t_type){
+    weatherData input;
+
+    cout << "Enter total rainfall(inches) for " << enumMonth(mth) << ": ";
+    d_in(&input.rainfall,1); //get rainfall
+
+    cout << "Enter max temperature("<< t_type <<") for " << enumMonth(mth) << ": ";
+    d_in(&input.h_temp); //get high temp
+    while(input.h_temp < -100 || input.h_temp > 140){
+        cout << "Temperature must be between -100 and 140 F, try again: ";
+        d_in(&input.h_temp);} //in range
+
+    cout << "Enter lowest temperature("<< t_type <<") for " << enumMonth(mth) << ": ";
+    d_in(&input.l_temp); //get low temp
+    while(input.l_temp < -100 || input.l_temp > 140){
+        cout << "Temperature must be between -100 and 140 F, try again: ";
+        d_in(&input.l_temp);} //in range
+
+    if (input.h_temp < input.l_temp){ //if the low is higher than the high
+        cout << "High and Low reversed, fixed that for you." << endl;
+        double temp = input.l_temp;
+        input.l_temp = input.h_temp;
+        input.h_temp = temp;
+    }
+
+    input.avg_temp = (input.h_temp+input.l_temp)/2; //calc average
+
+    return input; //return the structure
+} //getWeatherData
+
+//displays a line of weather data
+void displayWeatherData(const weatherData output, const int mth, const int year){
+    cout << left << fixed << setprecision(2) << setw(6) << year \
+    << setw(10) << enumMonth(mth) \
+    << setw(7) << output.rainfall \
+    << setw(7) << output.h_temp \
+    << setw(7) << output.l_temp \
+    << setw(7) << output.avg_temp << endl;
+} //displayWeatherData
--- CS102_Projects/C11/Marcum_Circle.cpp
+++ CS102_Projects/C11/Marcum_Circle.cpp
@@ -16,7 +16,7 @@ using namespace std;
 //Base Shape Class
 class Shape {
 public:
-    virtual double getArea(){};
+    virtual double getArea()=0;
 };
 
 //Circle Class
@@ -29,21 +29,20 @@ public:
     Circle(double r){radius = r;}
     void setRadius(double r){radius = r;}
     double getRadius(){return radius;}
-    double getArea(){return pi*pow(2,radius);}
+    double getArea(){return (pi*radius*radius);}
 };
 
 int main(){
     int shape_loop=0;
     double temp_shape;
-    vector<Shape> your_shapes;
+    vector<Circle> your_shapes;
     cout << "Number of shapes? ";
     cin >> shape_loop;
-
-    while(shape_loop){
+    for (int i=0;i<shape_loop;i++){
         cout << "Value for shape: ";
         cin >> temp_shape;
         your_shapes.push_back(Circle(temp_shape));
-        shape_loop--;
+        cout << your_shapes[i].getArea() << endl;
     }
     return 0;
 }
