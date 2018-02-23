cd73f1885f3ebacc19c70760dea1e9e98910c295
James <jemarcum386@gmail.com>
2018-02-23 06:55:45 -0500

    finished?
    put shape class in own file, added display, added sorting by shape and area.

--- CS102_Projects/C11/Marcum_Circle.cpp
+++ CS102_Projects/C11/Marcum_Circle.cpp
@@ -1,102 +1,45 @@
-/*
-Circle class - 1 member of radius - private
-public - functions - setRadius, getRadius, getArea, and getCircum
-prompt for radius - print radius area and circumference
-*/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                         *
+ * Author: James Marcum                                                    *
+ * Class: CS 102                                                           *
+ *                                                                         *
+ * Function: Circles & other fun Shapes                                    *
+ *           Get areas, sort things, fun for all Shapes                    *
+ *                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 //Did my best to make it flow and simple to follow
+//won't run on Quincy unless you turn on RTTI
+//but it worked fine with Code::Blocks automatically in testing
 
 #include <iostream>
 #include <iomanip>
 #include <vector>
 #include <string>
-#include <cmath>
+#include <typeinfo>
 #include <algorithm>
 #include "marcum_header.h"
 #include "marcum_header.cpp"
-//#include "Shapes.h"
+#include "Shapes.h"
 //namespace
 using namespace std;
 
-//Parent Shape Class
-class Shape {
-public:
-    virtual string getType() const = 0;
-    virtual double getArea() const = 0;
-    virtual double getPerimeter() const = 0;
-}; //Shape
 
-
-class Circle : public Shape{
-    //friend bool comp(const Circle & a, const Circle & b);
-private:
-    double pi, \
-        radius;
-
-public:
-    Circle() {radius = 1; pi = 3.14159;}
-    Circle(double r) {radius = r; pi = 3.14159;}
-    string getType() const {return "Circle";}
-    void setRadius(double r) {radius = r;}
-    void setArea(float a) {radius = sqrt(a/pi);}
-    void setDiameter(float d) {radius = d/2;}
-    void setCircum(float c) {radius = c/(2*pi);}
-    double getRadius() const {return radius;}
-    virtual double getArea() const {return (pi*radius*radius);}
-    double getCircum() const {return 2*pi*radius;}
-    double getPerimeter() const {return getCircum();} //circumference of a circle is it's perimeter
-}; //Circle
-
-
-class mRectangle : public Shape{
-private:
-    double length, \
-            width;
-public:
-    mRectangle() {length = 1; width = 1;}
-    mRectangle(double l, double w) {length = l; width = w;}
-    string getType() const {return "Rectangle";}
-    void setLength(double l) {length = l;}
-    void setWidth(double w) {width = w;}
-    double getLength() const {return length;}
-    double getWidth() const {return width;}
-    double getArea() const {return length * width;}
-    double getPerimeter() const {return length * 2 + width * 2;}
-}; //mRectangle
-//Had to make it mRectangle over Rectangle because it wouldn't compile as Rectangle
-
-
-class Triangle : public Shape{
-private:
-    double side1, side2, side3;
-public:
-    Triangle(double s1, double s2, double s3) {side1 = s1; side2 = s2; side3 = s3;}
-    //Right Triagle init below
-    Triangle(double leg1, double leg2) {side1 = leg1; side2 = leg2; side3 = sqrt((leg1*leg1)+(leg2*leg2));}
-    string getType() const {return "Triangle";}
-    void setSide1(double s) {side1 = s;}
-    void setSide2(double s) {side2 = s;}
-    void setSide3(double s) {side3 = s;}
-    double getPerimeter() const {return (side1 + side2 + side3)/2;}
-    double getArea() const {
-        double p = getPerimeter();
-        return sqrt(p*((p-side1)*(p-side2)*(p-side3)));}
-        //area of a triangle given only sides = perimeter - each side * perimeter
-    double getSide1() const {return side1;}
-    double getSide2() const {return side2;}
-    double getSide3() const {return side3;}
-}; //Triangle
-
-//bool comp(const Circle & a, const Circle & b) {return a.radius < b.radius;} //for sorting
+//==========
 //prototypes
 int mainLoop();
 int numberOfShapes(string type);
 int circleSetBy();
 int triSetBy();
-void sortShape(Shape &shape);
+int sortBy();
 void printArea(const Shape &shape);
 void printPerimeter(const Shape &shape);
 void printType(const Shape *shape);
+void displayShape(const Shape *shape);
+bool compareType(const Shape *typeA, const Shape *typeB);
+bool comp(const Shape *a, const Shape *b);
 
+//==============================================================================
 //BEGIN MAIN
 int main(){
     int selection = 1;
@@ -166,7 +109,7 @@ int main(){
         } //selection 2
 
         if (selection == 3) {
-            double side1,side2,side3;
+            double side1=0,side2=0,side3=0;
             int setTri = triSetBy();
             /*
             1 = Right triangle (2 side)
@@ -183,19 +126,49 @@ int main(){
                     shapes.push_back(new Triangle(side1,side2));
                 } //set by 1
                 if (setTri == 2){
-                    cout << "Enter Side 1 of Triangle #" << i+1 << ": ";
-                    d_in(&side1,1);
-                    cout << "Enter Side 2 of Triangle #" << i+1 << ": ";
-                    d_in(&side2,1);
-                    cout << "Enter Side 3 of Triangle #" << i+1 << ": ";
-                    d_in(&side3,1);
+                    do {
+                        cout << "Enter Side 1 of Triangle #" << i+1 << ": ";
+                        d_in(&side1,1);
+                        cout << "Enter Side 2 of Triangle #" << i+1 << ": ";
+                        d_in(&side2,1);
+                        cout << "Enter Side 3 of Triangle #" << i+1 << ": ";
+                        d_in(&side3,1);
+                        if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1)
+                            cout << "Invalid Triangle. Check length of sides." << endl;
+                    } while (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1);
+                    //triangle validation added
                     shapes.push_back(new Triangle(side1,side2,side3));
                 } //set by 2
             } //loop of shape
         } //selection 3
 
         if (selection == 4){
-            for_each(shapes.begin(),shapes.end(), printType);
+            int sortType = sortBy();
+            if (sortType == 1)
+                sort(shapes.begin(), shapes.end(),compareType); //sorts by shape
+            if (sortType == 2)
+                sort(shapes.begin(), shapes.end(),comp); //sorts by area
+            setcolor(2);
+            cout<< left << endl \
+                << setw(12) << "Shape" \
+                << setw(10) << "Radius /" \
+                << setw(10) << " " \
+                << setw(15) << "Side 3 /" \
+                << setw(10) << " " \
+                << setw(15) << "Perimeter /";
+            cout<< left << endl \
+                << setw(12) << "Type" \
+                << setw(10) << "Length" \
+                << setw(10) << "Width" \
+                << setw(15) << "Hypotenuse" \
+                << setw(10) << "Area" \
+                << setw(15) << "Circumference" << endl;
+
+            setcolor(5);
+            for (int i = 0; i < 80; i++)
+                cout << "_";
+            setcolor();
+            for_each(shapes.begin(),shapes.end(), displayShape);
         getchar();
         } // selection 4
 
@@ -203,7 +176,10 @@ int main(){
 
     return 0;
 } //END MAIN
+//==============================================================================
 
+//==============================================================================
+//BEGIN Functions
 
 //main loop - main display and selection getter for loop
 int mainLoop(){
@@ -213,7 +189,7 @@ int mainLoop(){
     cout<< "Options:" << endl \
         << "A: Create Circles" << endl \
         << "B: Create Rectangles" << endl \
-        << "C: Create Triagles" << endl \
+        << "C: Create Triangles" << endl \
         << "D: Display Shapes" << endl \
         << "E: Exit" << endl << endl;
     cout << "Select an Option from the list: ";
@@ -255,8 +231,8 @@ int circleSetBy(){
 }//circleSetBy
 
 int triSetBy(){
-    char option, options[] = {'a','b','c','d'};
-    cout<< endl << "Triagle type:" << endl \
+    char option, options[] = {'a','b'};
+    cout<< endl << "Triangle type:" << endl \
         << "A: Right Triangle (Set with length of non-hypo sides)" << endl \
         << "B: Other Triangle (Sets by length of 3 sides)" << endl;
     cout<< "Create Triangle Using: ";
@@ -268,22 +244,84 @@ int triSetBy(){
     return 0;
 }//triSetBy
 
-void sortShape(Shape &shape){
+int sortBy(){
+    char option, options[] = {'a','b'};
+    cout<< endl << "Sort Options:" << endl \
+        << "A: Shape Type" << endl \
+        << "B: Area" << endl;
+    cout<< "Sort by: ";
+    c_in(&option,options,2);
+    switch(option){
+        case('a'): return 1;
+        case('b'): return 2;
+    }
+    return 0;
+}//sortBy
 
-
-} //sortShape
-
+//this set of print functions were mostly used for testing
 //prints area of shape
 void printArea(const Shape &shape){
     cout << shape.getArea();
-}
+}//printArea
 
 //prints perimeter of shape
 void printPerimeter(const Shape &shape){
     cout << shape.getPerimeter();
-}
+}//printPerimeter
 
 //prints type of shape
 void printType(const Shape *shape){
     cout << shape->getType() << endl;
+}//printType
+
+//displays the attributes of a given shape
+void displayShape(const Shape *shape){
+    //sets color by type
+    if (shape->getType() == "Circle")
+        setcolor(11);
+    if (shape->getType() == "Rectangle")
+        setcolor(13);
+    if (shape->getType() == "Triangle")
+        setcolor(12);
+
+    cout << left << fixed << setprecision(2) \
+    << setw(12) << shape->getType();
+    cout << shape->getAttributes();
+    cout << setw(10) << shape->getArea() \
+    << setw(15) << shape->getPerimeter() << endl;
 }
+
+//compares type of shape for sorting purposes
+//probably could have made it simpler, but it works
+bool compareType(const Shape *typeA, const Shape *typeB){
+    int A, B;
+
+    if (typeid(*typeA) == typeid(Circle))
+        A = 0;
+    if (typeid(*typeB) == typeid(Circle))
+        B = 0;
+
+    if (typeid(*typeA) == typeid(mRectangle))
+        A = 1;
+    if (typeid(*typeB) == typeid(mRectangle))
+        B = 1;
+
+    if (typeid(*typeA) == typeid(Triangle))
+        A = 2;
+    if (typeid(*typeB) == typeid(Triangle))
+        B = 2;
+
+    if (A == B) //if same type sort by area
+        return comp(typeA, typeB);
+
+    return A < B;
+} //compareType
+
+//compares a given shape by area
+bool comp(const Shape *a, const Shape *b){
+    double A = a->getArea(), B = b->getArea();
+    return A < B;
+} //comp
+
+//END Functions
+//==============================================================================
--- CS102_Projects/C11/Shapes.h
+++ CS102_Projects/C11/Shapes.h
@@ -3,6 +3,12 @@
 #ifndef SHAPES_HEADER
 #define SHAPES_HEADER
 //============================
+//just left this one with all inline declarations because they are mostly single line functions
+
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cmath>
 
 //Parent Shape Class
 class Shape {
@@ -10,11 +16,11 @@ public:
     virtual string getType() const = 0;
     virtual double getArea() const = 0;
     virtual double getPerimeter() const = 0;
+    virtual string getAttributes() const = 0;
 }; //Shape
 
 
 class Circle : public Shape{
-    //friend bool comp(const Circle & a, const Circle & b);
 private:
     double pi, \
         radius;
@@ -23,32 +29,43 @@ public:
     Circle() {radius = 1; pi = 3.14159;}
     Circle(double r) {radius = r; pi = 3.14159;}
     string getType() const {return "Circle";}
+    string getAttributes() const {
+        stringstream ss;
+        ss << fixed << setprecision(2) << left << setw(10) << radius << setw(25) << " ";
+        return ss.str();
+    }
     void setRadius(double r) {radius = r;}
     void setArea(float a) {radius = sqrt(a/pi);}
     void setDiameter(float d) {radius = d/2;}
     void setCircum(float c) {radius = c/(2*pi);}
     double getRadius() const {return radius;}
-    virtual double getArea() const {return (pi*radius*radius);}
+    double getArea() const {return (pi*radius*radius);}
     double getCircum() const {return 2*pi*radius;}
     double getPerimeter() const {return getCircum();} //circumference of a circle is it's perimeter
 }; //Circle
 
 
-class Rectangle : public Shape{
+class mRectangle : public Shape{
 private:
     double length, \
             width;
 public:
-    Rectangle() {length = 1; width = 1;}
-    Rectangle(double l, double w) {length = l; width = w;}
-    string getType() {return "Rectangle";}
+    mRectangle() {length = 1; width = 1;}
+    mRectangle(double l, double w) {length = l; width = w;}
+    string getType() const {return "Rectangle";}
+    string getAttributes() const {
+        stringstream ss;
+        ss << fixed << setprecision(2) << left << setw(10) << length << setw(10) << width << setw(15) << " ";
+        return ss.str();
+    }
     void setLength(double l) {length = l;}
     void setWidth(double w) {width = w;}
     double getLength() const {return length;}
     double getWidth() const {return width;}
     double getArea() const {return length * width;}
     double getPerimeter() const {return length * 2 + width * 2;}
-}; //Rectangle
+}; //mRectangle
+//Had to make it mRectangle over Rectangle because it wouldn't compile as Rectangle
 
 
 class Triangle : public Shape{
@@ -56,15 +73,20 @@ private:
     double side1, side2, side3;
 public:
     Triangle(double s1, double s2, double s3) {side1 = s1; side2 = s2; side3 = s3;}
-    //Right Triagle init below
+    //Right triangle init below
     Triangle(double leg1, double leg2) {side1 = leg1; side2 = leg2; side3 = sqrt((leg1*leg1)+(leg2*leg2));}
     string getType() const {return "Triangle";}
+    string getAttributes() const {
+        stringstream ss;
+        ss << fixed << setprecision(2) << left << setw(10) << side1 << setw(10) << side2 << setw(15) << side3;
+        return ss.str();
+    }
     void setSide1(double s) {side1 = s;}
     void setSide2(double s) {side2 = s;}
     void setSide3(double s) {side3 = s;}
-    double getPerimeter() const {return (side1 + side2 + side3)/2;}
+    double getPerimeter() const {return (side1 + side2 + side3);}
     double getArea() const {
-        double p = getPerimeter();
+        double p = getPerimeter()/2;
         return sqrt(p*((p-side1)*(p-side2)*(p-side3)));}
         //area of a triangle given only sides = perimeter - each side * perimeter
     double getSide1() const {return side1;}
