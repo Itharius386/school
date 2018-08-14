e8a0ba64183571fd5443d6c9e3eec022e3ee2ee4
James <jemarcum386@gmail.com>
2018-08-14 11:18:11 -0400

    .
    .

--- CS102_Projects/C5&12/C12_P3.cpp
+++ CS102_Projects/C5&12/C12_P3.cpp
@@ -1,11 +1,89 @@
-/*
-Punch Line
-Write a program that reads and prints a joke and its punch line from two different files.
-The first file contains a joke, but not its punch line. The second file has the punch line as
-its last line, preceded by “garbage.” The main function of your program should open the
-two files and then call two functions, passing each one the file it needs. The first function
-should read and display each line in the file it is passed (the joke file). The second function
-should display only the last line of the file it is passed (the punch line file). It should find
-this line by seeking to the end of the file and then backing up to the beginning of the last
-line. Data to test your program can be found in the joke.txt and punchline.txt files.
-*/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                         *
+ *    Author: James Marcum                                                                 *
+ *    Class:  CS102                                                                        *
+ *                                                                                         *
+ *    Function: Tell a joke, Tell a punchline                                              *
+ *                                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "marcum_header.h"
+#include "marcum_header.cpp"
+using namespace std;
+
+int main() {
+fstream joke, punchline;
+string input, file = "joke.txt", file2 = "punchline.txt";
+char yn = 'z', check[] = {'y','n'};
+
+joke.open(file.c_str(), ios::in);
+if (joke.fail()) {
+    cout << "File Not Found" << endl << "Would you like to enter a file name? (y/n): ";
+    c_in(&yn,check,2);
+    if (yn == 'y') {
+        int attempt = 3;
+        do{
+            cout << endl << "Enter name of file to be opened (include .txt): ";
+            getline(cin, file);
+            joke.open(file.c_str(),ios::in);
+            if (joke.fail()) {
+                attempt--;
+                cout << "File Not Found - Try again. " << attempt << " attempts remaining" << endl;
+            }
+        if (attempt == 0)
+            break;
+        } while(joke.fail());
+    }
+    else if (yn == 'n' || joke.fail()) {
+        cout << "Exiting Program";
+        return 0;
+    }
+}
+
+while (joke) {
+    getline(joke, input);
+    cout << input << endl;
+}
+
+punchline.open(file2.c_str(), ios::in | ios::ate);
+if (punchline.fail()) {
+    cout << "File Not Found" << endl << "Would you like to enter a file name? (y/n): ";
+    c_in(&yn,check,2);
+    if (yn == 'y') {
+        int attempt = 3;
+        do{
+            cout << endl << "Enter name of file to be opened (include .txt): ";
+            getline(cin, file);
+            punchline.open(file.c_str(),ios::in | ios::ate);
+            if (punchline.fail()) {
+                attempt--;
+                cout << "File Not Found - Try again. " << attempt << " attempts remaining" << endl;
+            }
+        if (attempt == 0)
+            break;
+        } while(punchline.fail());
+    }
+    else if (yn == 'n' || punchline.fail()) {
+        cout << "Exiting Program";
+        return 0;
+    }
+}
+char ch = 'z';
+while (punchline && ch != '\n') {
+    for(long i = 1; i < 10000; i++) {
+        punchline.seekg(-i,ios::end);
+        punchline.get(ch);
+        if (ch == '\n')
+            break;
+    }
+}
+    getline(punchline, input);
+    cout << input << endl << endl;
+
+
+return 0;
+}
--- CS102_Projects/C5&12/C5_P24.cpp
+++ CS102_Projects/C5&12/C5_P24.cpp
@@ -1,13 +1,59 @@
-/*
-Using Files—Numeric Processing
-If you have downloaded this book’s source code from the companion Web site, you
-will find a file named Random.txt in the Chapter 05 folder. (The companion Web
-site is at www.pearsonhighered.com/gaddis .) This file contains a long list of random
-numbers. Copy the file to your hard drive and then write a program that opens the file,
-reads all the numbers from the file, and calculates the following:
-A) The number of numbers in the file
-B) The sum of all the numbers in the file (a running total)
-C) The average of all the numbers in the file
-The program should display the number of numbers found in the file, the sum of the
-numbers, and the average of the numbers.
-*/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                         *
+ *    Author: James Marcum                                                                 *
+ *    Class:  CS102                                                                        *
+ *                                                                                         *
+ *    Function: Reads in the numbers of a file and produces the sum, count, and Average    *
+ *                                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "marcum_header.h"
+#include "marcum_header.cpp"
+using namespace std;
+
+int main() {
+fstream num_file;
+string file = "Random.txt";
+double sum = 0, average = 0, input;
+int count = 0;
+char yn = 'z', check[] = {'y','n'};
+
+num_file.open(file.c_str(),ios::in);
+if (num_file.fail()) {
+    cout << "File Not Found" << endl << "Would you like to enter a file name? (y/n): ";
+    c_in(&yn,check,2);
+    if (yn == 'y') {
+        int attempt = 3;
+        do{
+            cout << endl << "Enter name of file to be opened (include .txt): ";
+            getline(cin, file);
+            num_file.open(file.c_str(),ios::in);
+            if (num_file.fail()) {
+                attempt--;
+                cout << "File Not Found - Try again. " << attempt << " attempts remaining" << endl;
+            }
+        if (attempt == 0)
+            break;
+        } while(num_file.fail());
+    }
+    else if (yn == 'n' || num_file.fail()) {
+        cout << "Exiting Program";
+        return 0;
+    }
+}
+    while(num_file >> input){
+        sum += input;
+        count++;
+    }
+    num_file.close();
+    average = sum / count;
+    cout << "Sum of numbers in file: " << sum << endl;
+    cout << "Average of numbers in file: " << average << endl;
+    cout << "Count of numbers in file: " << count << endl;
+
+return 0;
+}
